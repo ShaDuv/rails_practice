cb4144e0c89ef82e9ae8ad0d5022559f98dc6e3d
Student <example@example.com>
2019-08-12 15:12:48 -0700

    seed the database but still has issues with using the grandparent id as a secondary foreign key

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,32 @@
 User.destroy_all
 Topic.destroy_all
 Answer.destroy_all
-25.times do |index|
-  User.create!(user_name: Faker::TvShows::Simpsons.character)
-  rand(1...4).times do |middex|
+# 25.times do |index|
+#   users = User.create!(user_name: Faker::TvShows::Simpsons.character)
+#   rand(1...4).times do |index|
+#     question = Faker::Lorem.question(word_count: 4, supplemental: false, random_words_to_add: 3)
+#     topic = Topic.new(:question => question)
+#     topic.user = topic.save!
+#   #   rand(5...17).times do |pokedex|
+#   #     Answer.create!(text_body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote)
+#   #   end
+#   end
+# end
+
+
+25.times do
+  user = User.new(:user_name => Faker::TvShows::Simpsons.character)
+  5.times do
     question = Faker::Lorem.question(word_count: 4, supplemental: false, random_words_to_add: 3)
-    topic = Topic.new(question)
-    topic.user = topic.save!
-  #   rand(5...17).times do |pokedex|
-  #     Answer.create!(text_body: Faker::Movies::HitchhikersGuideToTheGalaxy.quote)
-  #   end
+    topic = user.topics.new(:question => question)
+    topic.save
+    5.times do
+      answer_content = Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+      answer = topic.answers.new(:text_body => answer_content, :user_id => user.id)
+      answer.save
+    end
   end
 end
-
 
 
 
